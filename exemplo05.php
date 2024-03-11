a7d0082f3009cd95cff7f412d9a48d5c9aa3fd1a
drago1605 <matheusdrago2018@gmail.com>
2024-03-11 20:15:00 -0300

    trabalhando com escopo de função de script

---
+++ exemplo05.php
@@ -0,0 +1,16 @@
+<?php 
+
+$nome = "Armandino";
+
+function exibir(){
+      $nome = "josefina";
+
+      echo "valor da variavel dentro da função " .$nome;
+
+}
+
+ exibir();
+
+ echo "<br /> Valor da varivel fora da função " . $nome;
+
+ ?>
